ba85bb88d4acd44cf9721383206bcaced0ce9fb6
neelscloud <godkarneelam@gmail.com>
2025-11-14 15:31:54 +0530

    helo

--- avg10.c
+++ avg10.c
@@ -9,7 +9,7 @@ int main()
     for(i=0;i<n;i++)
         scanf("%d",&a[i]);
     for(i=0;i<n;i++)
-        sum+=a[i];bfdc
+        sum+=a[i];
     printf("\n the sum of %d students marks is %f and its average is %f",n,sum,sum/n);
     return 0;
 }
